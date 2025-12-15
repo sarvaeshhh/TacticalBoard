924d4abe8de8e8dd868ac448a865c19d142b6180
sarvaeshhh <sarvaesh.raam19@gmail.com>
2025-12-15 21:59:34 +0530

    fix: allow all hosts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: true, // <--- THIS IS THE MAGIC LINE
       },
       plugins: [react(), tailwindcss()],
       define: {
